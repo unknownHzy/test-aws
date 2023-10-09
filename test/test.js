bbf415518303e661e4b0e0495ad38aefa580cce1
JoyHu <25299299+unknownHzy@users.noreply.github.com>
2023-10-09 22:19:18 +0800

    add UT

---
+++ test/test.js
@@ -0,0 +1,9 @@
+const assert = require("assert");
+
+describe('api', function () {
+    describe('GET /api/users groupA', function () {
+        it('respond with an array of users', function () {
+            assert.equal(1, 1)
+        });
+    });
+});
