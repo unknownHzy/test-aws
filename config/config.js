c73cd468f0650bf41c3b080e8a274828f26add13
JoyHu <25299299+unknownHzy@users.noreply.github.com>
2023-10-12 14:16:03 +0800

    inject env var

---
+++ config/config.js
@@ -0,0 +1,34 @@
+require('dotenv').config();
+
+module.exports = {
+  "development": {
+      "username": process.env.DB_USERNAME,
+      "password": process.env.DB_PASSWORD,
+      "database": process.env.DB_DATABASE,
+      "host": "localhost",
+      "dialect": "mysql",
+      "port": 3306,
+      "seederStorage": "sequelize",
+      "seederStorageTableName": "seeders_data"
+  },
+  "test": {
+      "username": "root",
+      "password": "root",
+      "database": "test",
+      "host": "localhost",
+      "dialect": "mysql",
+      "port": 3306,
+      "seederStorage": "sequelize",
+      "seederStorageTableName": "seeders_data"
+  },
+  "production": {
+      "username": process.env.DB_USERNAME,
+      "password": process.env.DB_PASSWORD,
+      "database": process.env.DB_DATABASE,
+      "host": process.env.DB_HOST,
+      "dialect": "mysql",
+      "port": 3306,
+      "seederStorage": "sequelize",
+      "seederStorageTableName": "seeders_data"
+  }
+}
